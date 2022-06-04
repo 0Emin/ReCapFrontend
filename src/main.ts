2c68025f846964eae3f97c0ba21e186aa2a91999
Mehmet Emin Pala <mehmetemin.mp99@gmail.com>
2022-06-04 17:07:11 +0300

    04.06.2022

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+                // hangi module le başlayayım olaya: AppModule
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
