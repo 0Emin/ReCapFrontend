25c19dd246f03b4f594f8a1f3d8d3b53d1f35d4a
Mehmet Emin Pala <mehmetemin.mp99@gmail.com>
2022-06-09 22:48:22 +0300

    09.06.2022

---
+++ src/app/services/brand.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Brand } from '../models/brand';
+import { ListResponseModel } from '../models/ListResponseModel';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BrandService {
+
+  apiUrl = "https://localhost:44341/api/brands/getall"
+ 
+  constructor(private httpClient:HttpClient) { }
+
+  getBrands():Observable<ListResponseModel<Brand>>{
+     return this.httpClient.get<ListResponseModel<Brand>>(this.apiUrl)
+  }
+}
